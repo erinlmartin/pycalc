3ebdf6a64d0142eca4f6b66c76e3e02cbd02de6a
erinlmartin <erin.l.martin@postgrad.curtin.edu.au>
2020-09-14 15:18:19 +1000

    Add documentation

--- code1.py
+++ code1.py
@@ -1,5 +1,10 @@
+# Perform simple arithmetic encoded in an input string:
+# '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
-    num0, operator, num1 = expression.split(' ')
+    values = expression.split(' ')
+    num0 = int(values[0])
+    operator = values[1]
+    num1 = int(values[2])
     if operator == '+':
         return num0 + num1
     elif operator == '-':
@@ -7,5 +12,4 @@ def compute(expression):
     else:
         print('unknown operator!')
         return None
-
 # use present tense for messaes on GIT!!
